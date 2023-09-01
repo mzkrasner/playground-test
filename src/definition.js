a46a3c75c2303f505b274d6cb6c9db6c064aaf85
Mark Krasner <markkrasner@Marks-MacBook.local>
2023-09-01 11:22:27 -0400

    Changing to new dataset

--- src/definition.js
+++ src/definition.js
@@ -1,2 +1,115 @@
 // This is an auto-generated file, do not edit manually
-export const definition = {"models":{"User":{"id":"kjzl6hvfrbw6c9ww1ru9ytsb4ymnn3pyadv9qn0c3qe7z3z19r29lhdsodwulea","accountRelation":{"type":"single"}},"ForumPost":{"id":"kjzl6hvfrbw6c770ap2x7o517242i539hr5382lzj8vqulrk5hhgmt80bl86auh","accountRelation":{"type":"list"}},"Comment":{"id":"kjzl6hvfrbw6ca13nnlfi0na3uml83ki2dv0jrwvoek6h37wbcz3d6kckgealyk","accountRelation":{"type":"list"}}},"objects":{"User":{"lastName":{"type":"string","required":true},"firstName":{"type":"string","required":true},"author":{"type":"view","viewType":"documentAccount"},"forumPosts":{"type":"view","viewType":"relation","relation":{"source":"queryConnection","model":"kjzl6hvfrbw6c770ap2x7o517242i539hr5382lzj8vqulrk5hhgmt80bl86auh","property":"userId"}}},"ForumPost":{"userId":{"type":"streamid","required":true},"summary":{"type":"string","required":true},"category":{"type":"string","required":false},"user":{"type":"view","viewType":"relation","relation":{"source":"document","model":"kjzl6hvfrbw6c9ww1ru9ytsb4ymnn3pyadv9qn0c3qe7z3z19r29lhdsodwulea","property":"userId"}},"author":{"type":"view","viewType":"documentAccount"},"version":{"type":"view","viewType":"documentVersion"},"comments":{"type":"view","viewType":"relation","relation":{"source":"queryConnection","model":"kjzl6hvfrbw6ca13nnlfi0na3uml83ki2dv0jrwvoek6h37wbcz3d6kckgealyk","property":"forumPostID"}}},"Comment":{"comment":{"type":"string","required":true},"forumPostID":{"type":"streamid","required":true},"author":{"type":"view","viewType":"documentAccount"},"version":{"type":"view","viewType":"documentVersion"},"forumPost":{"type":"view","viewType":"relation","relation":{"source":"document","model":"kjzl6hvfrbw6c770ap2x7o517242i539hr5382lzj8vqulrk5hhgmt80bl86auh","property":"forumPostID"}}}},"enums":{},"accountData":{"user":{"type":"node","name":"User"},"forumPostList":{"type":"connection","name":"ForumPost"},"commentList":{"type":"connection","name":"Comment"}}}
+export const definition = {
+    models: {
+      BasicProfile: {
+        id: "kjzl6hvfrbw6c8i6k87cma2gvbnw3hgz2j2tc0hmc6yzzc1zikbwsryulsnmmak",
+        accountRelation: { type: "single" },
+      },
+      Posts: {
+        id: "kjzl6hvfrbw6cags7lvf5ajsgdfqwnnhoyp5fr3fhmitskk0ksydeaspzxckpub",
+        accountRelation: { type: "list" },
+      },
+      Following: {
+        id: "kjzl6hvfrbw6c930807bvmzvcqkqv6ni1q2jhv5bc59hima9r9ci1zk22tpxq3v",
+        accountRelation: { type: "list" },
+      },
+      Comments: {
+        id: "kjzl6hvfrbw6cb3x190263pxlza37ls1gxizt1ea21gtr9xwp7qln05elotk0h8",
+        accountRelation: { type: "list" },
+      },
+    },
+    objects: {
+      BasicProfile: {
+        name: { type: "string", required: true },
+        emoji: { type: "string", required: false },
+        gender: { type: "string", required: false },
+        username: { type: "string", required: true },
+        description: { type: "string", required: false },
+        posts: {
+          type: "view",
+          viewType: "relation",
+          relation: {
+            source: "queryConnection",
+            model:
+              "kjzl6hvfrbw6cags7lvf5ajsgdfqwnnhoyp5fr3fhmitskk0ksydeaspzxckpub",
+            property: "profileId",
+          },
+        },
+      },
+      Posts: {
+        tag: { type: "string", required: true, indexed: true },
+        body: { type: "string", required: true },
+        edited: { type: "datetime", required: false, indexed: true },
+        created: { type: "datetime", required: true, indexed: true },
+        profileId: { type: "streamid", required: true },
+        profile: {
+          type: "view",
+          viewType: "relation",
+          relation: {
+            source: "document",
+            model:
+              "kjzl6hvfrbw6c8i6k87cma2gvbnw3hgz2j2tc0hmc6yzzc1zikbwsryulsnmmak",
+            property: "profileId",
+          },
+        },
+        comments: {
+          type: "view",
+          viewType: "relation",
+          relation: {
+            source: "queryConnection",
+            model:
+              "kjzl6hvfrbw6cags7lvf5ajsgdfqwnnhoyp5fr3fhmitskk0ksydeaspzxckpub",
+            property: "postId",
+          },
+        },
+      },
+      Following: {
+        profileId: { type: "streamid", required: true },
+        profile: {
+          type: "view",
+          viewType: "relation",
+          relation: {
+            source: "document",
+            model:
+              "kjzl6hvfrbw6c8i6k87cma2gvbnw3hgz2j2tc0hmc6yzzc1zikbwsryulsnmmak",
+            property: "profileId",
+          },
+        },
+      },
+      Comments: {
+        edited: { type: "datetime", required: false, indexed: true },
+        postId: { type: "streamid", required: true },
+        comment: { type: "string", required: true },
+        created: { type: "datetime", required: true, indexed: true },
+        profileId: { type: "streamid", required: true },
+        post: {
+          type: "view",
+          viewType: "relation",
+          relation: {
+            source: "document",
+            model:
+              "kjzl6hvfrbw6cags7lvf5ajsgdfqwnnhoyp5fr3fhmitskk0ksydeaspzxckpub",
+            property: "postId",
+          },
+        },
+        profile: {
+          type: "view",
+          viewType: "relation",
+          relation: {
+            source: "document",
+            model:
+              "kjzl6hvfrbw6c8i6k87cma2gvbnw3hgz2j2tc0hmc6yzzc1zikbwsryulsnmmak",
+            property: "profileId",
+          },
+        },
+      },
+    },
+    enums: {},
+    accountData: {
+      basicProfile: { type: "node", name: "BasicProfile" },
+      postsList: { type: "connection", name: "Posts" },
+      followingList: { type: "connection", name: "Following" },
+      commentsList: { type: "connection", name: "Comments" },
+    },
+  };
+  
